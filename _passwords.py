856b2b2603a80184c25a84d5e5094c9148a620d1
kenyounge <KenYounge@Gmail.com>
2015-01-19 22:20:28 -0500

    Change example to one of passwords.

---
+++ _passwords.py
@@ -0,0 +1,5 @@
+#This file is included for demo purposes only - YOU SHOULD EXCLUDE NON-ENCRYPTED PASSWORDS FROM YOUR GIT REPO.
+MYSQL_PWD = 'MyExample!password4'
+SENDGRID_PWD = 'THisIS_my44sendgridpwd'
+LOGGLY_URL = 'http://logs-01.loggly.com/inputs/00-00-00-00-00/'
+HELLO_WORLD = 'Hello world - my passwords are secret!'
--- _settings.py
+++
@@ -1,5 +0,0 @@
-"""Note: this is in the git repo for demonstration purposes. Typically exclude the raw file from the repo."""
-MYSQL_PASSWORD = 'MyExample!password4'
-SENDGRID_PWD = 'THisIS_my44sendgridpwd'
-LOGGLY_URL = 'http://logs-01.loggly.com/inputs/00-00-00-00-00/'
-HELLO_WORLD_TEST = 'Hello world - my passwords are secret!'
